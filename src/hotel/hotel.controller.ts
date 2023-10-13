d76539eedb2ea24d236ee59bc4a96dc49d64c5e9
ayushverma123 <ayushv657@gmail.com>
2023-10-13 16:12:02 +0530

    Update hotel.controller.ts

--- src/hotel/hotel.controller.ts
+++ src/hotel/hotel.controller.ts
@@ -1,9 +1,12 @@
+
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
 import { ApiOkResponse, ApiNotFoundResponse } from '@nestjs/swagger';
 import { ApiCreatedResponse, ApiForbiddenResponse } from '@nestjs/swagger';
-import { UseFilters } from '@nestjs/common/decorators';
+import { UploadedFile, UseFilters, UseInterceptors } from '@nestjs/common/decorators';
 import { UsePipes } from '@nestjs/common/decorators';
 import { ValidationPipe } from '@nestjs/common/pipes';
-import { Controller, Get, Post, Put, Delete, Param, Body, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Query, UseGuards } from '@nestjs/common';  
 import { HotelService } from './hotel.service';
 import { CreateHotelDto } from './dto/createHotel-dto';
 import { GetQueryDto } from './dto/query-dto';
@@ -16,13 +19,53 @@ import { Admin } from 'mongodb';
 @ApiTags('Hotels')
 @Controller('hotels')
 export class HotelController {
-  constructor(private readonly hotelService: HotelService) { }
-  
+  constructor(private readonly hotelService: HotelService) { }  
+
   @Roles('Admin')
   @UseGuards(JwtGuard, RolesGuard)
   @Get('getall')
-  async getHotels(@Query() queryDto: GetQueryDto): Promise<any> {
+  @UsePipes(new ValidationPipe())
+  async getHotels(@Query() queryDto: GetQueryDto): Promise<any> {    
     return this.hotelService.getFilteredHotels(queryDto);
+  }
+  
+    @Post('create')
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: "./uploads",
+            filename: (req, file, cb) => {
+                const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+                cb(null, `${uniqueSuffix}-${file.originalname}`);
+            }
+        })
+    }))
+
+    async createHotel1(
+        @UploadedFile() file: Express.Multer.File, // Access the uploaded file if needed
+        @Body() createHotelDto: CreateHotelDto
+    ): Promise<HotelInterfaceResponse | null> {
+        if (file) {
+            // If an image file is uploaded, you can handle it here
+            // For example: save the file path to the database along with other data
+            const imagePath = file.path;
+            createHotelDto.image = imagePath;
+        }
+        
+        return this.hotelService.createHotel(createHotelDto);
+    }
+  
+  @Post("/upload")
+  @UseInterceptors(FileInterceptor('file' , {
+    storage : diskStorage({
+      destination : "./uploads",
+      filename : (req , file , cb) => {
+        cb(null , `${file.originalname}`)
+      }
+    })
+  }))
+  async uploadFile(@UploadedFile() file : any) {
+    console.log(file);
+    return "success";
   }
 
   @UseGuards(JwtGuard)
@@ -32,7 +75,7 @@ export class HotelController {
   async getHotelById(@Param('id') id: string): Promise<HotelInterfaceResponse | null> {
     return this.hotelService.getHotelById(id);
   }
-
+   
   @UseGuards(JwtGuard)
   @ApiCreatedResponse({ description: 'The record has been successfully created.' })
   @ApiForbiddenResponse({ description: 'Forbidden.' })
