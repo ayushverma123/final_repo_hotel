8a80bf59319a93e37b40189dad7679cba549578c
ayushverma123 <ayushv657@gmail.com>
2023-10-16 15:03:59 +0530

    Add files via upload

---
+++ src/Enquiry/enquiry.module.ts
@@ -0,0 +1,18 @@
+import { EnquirySchema } from 'src/entities/enquiry.entity'; 
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+import { EnquiryController } from './enquiry.controller';
+import { EnquiryService } from './enquiry.service';
+import { Enquiry } from 'src/entities/enquiry.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Enquiry.name, schema: EnquirySchema }
+   ]),
+ ],
+  controllers: [EnquiryController],
+  providers: [EnquiryService]
+
+})
+export class EnquiryModule { } 
